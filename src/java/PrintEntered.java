bddaf0b762f50fad81df4b17fc4c137fa178fb10
saiganeshsunkari <saiganeshsunkari38@gmail.com>
2016-06-01 10:20:13 -0400

    test 1

---
+++ src/java/PrintEntered.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ *
+ * @author Miracle
+ */
+public class PrintEntered {
+    public static String value(String s)
+    {
+        return s;
+    }
+    public static void main(String[] args) {
+        System.out.println(value("s"));
+    }
+    
+}
