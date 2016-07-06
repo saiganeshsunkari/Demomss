84abff0b631710f8ff43e80fbaf3b300eb2b66d9
saiganeshsunkari <saiganeshsunkari38@gmail.com>
2016-07-06 15:00:52 -0400

    Update PrintEntered.java

--- src/java/PrintEntered.java
+++ src/java/PrintEntered.java
@@ -10,12 +10,9 @@ package javapractice;
  * @author Miracle
  */
 public class PrintEntered {
-    public static String value(String s)
-    {
-        return s;
-    }
+    
     public static void main(String[] args) {
-        System.out.println(value("s"));
+        System.out.println("Hi ganesh");
     }
     
 }
